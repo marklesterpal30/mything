152905e3669602180265b1a4958132e981694c87
Mark Lester <marklestermarananpal@gmail.com>
2025-10-15 17:28:42 +0800

    install chakraui and setup it to the project

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -1,5 +1,14 @@
+import { Button, Text } from "@chakra-ui/react";
+
 const Sample = () => {
-	return <div>this is sample</div>;
+	return (
+		<div>
+			<Button colorScheme="blue">Button</Button>
+			<Text fontSize="4xl" fontWeight="medium" color="red.400">
+				Sample Component
+			</Text>
+		</div>
+	);
 };
 
 export default Sample;
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import App from "./App";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<RouterProvider router={router} />
+		</ChakraProvider>
 	</React.StrictMode>
 );
